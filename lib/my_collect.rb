26a307b9d701e90d6891519121dc1b1b57176f96
Solangel Lagares <sp.lagares31@gmail.com>
2020-09-19 18:33:05 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(students)
   pupil = [ ]
   counter = 0
       while counter < students.length 
-      pupil << students[counter]
+      pupil << yield(students[counter])
       counter += 1
       end
     pupil 
