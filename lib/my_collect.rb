40304afab0d2609b5eb8df4af936b61b8ea65bb9
Solangel Lagares <sp.lagares31@gmail.com>
2020-09-19 17:58:52 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+def my_collect(students)
+  students = [ ]
+  counter = 0
+      my_collect(students) do |i|
+      counter += 1
+      end
+    i =+ 1 
+end
 
+def my_collect(languages)
+    my_collect(languages) do |language|
+    end
+end
+ 
+
+  
 
